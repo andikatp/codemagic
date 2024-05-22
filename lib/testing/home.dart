48843e30b90ba96dbc9cee4569e7327d9b1baab8
andikatp <triprasetya_andika@yahoo.com>
2024-05-22 09:40:18 +0700

    test oatch

--- lib/testing/home.dart
+++ lib/testing/home.dart
@@ -35,7 +35,7 @@ class _HomeState extends State<Home> {
                   () => pageIndexNotifier.value = pageIndexNotifier.value + 1,
                 );
               },
-              child: const Text('Next'),
+              child: const Text('Nextaa'),
             ),
           ],
         ),
